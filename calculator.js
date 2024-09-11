cb619d3ff345102d83e0e11b6f1d46aca277407d
Brahmini <brahminigaddipati81@mail.com>
2024-09-11 10:55:51 +0530

    initial

---
+++ calculator.js
@@ -0,0 +1,12 @@
+function calculator(add,sub,div,mul){
+    return add;
+    return sub;
+    return div;
+    return mul;
+}
+function add(a,b){
+    return a+b;
+}
+calculator()
+let sum=add(10+10);
+console.log(sum);
---
+++ it.js
@@ -0,0 +1,4 @@
+let a = "shrey"
+let b = a;
+console.log(a)
+console.log(b)
